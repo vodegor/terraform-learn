e27637e15b3571ab9e87d8d8954c8cd92f4e34cd
Egor Vodnev <e.vodnev@tinkoff.ru>
2022-10-07 22:29:58 +0300

    push bash script

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "example" {
   subnet_id     = "subnet-0682ae7ae47957e6a"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-example"
   }
